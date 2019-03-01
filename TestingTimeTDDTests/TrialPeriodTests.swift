efb43a697fa3b290911eb69aa9d5c1fa4d96440e
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-01 15:44:33 -0500

    Test trial period.

--- TestingTimeTDDTests/TrialPeriodTests.swift
+++ TestingTimeTDDTests/TrialPeriodTests.swift
@@ -11,22 +11,47 @@ import XCTest
 // Test code in your main module when dynamically linking unit tests
 //@testable import TestingTimeTDD
 
+typealias Clock = () -> Date
+
+class TrialPeriod {
+    var dateInstalled: Date
+    var dateGenerator: Clock
+    
+    init(dateGenerator: @escaping Clock){
+        self.dateInstalled = dateGenerator()//Generate a date installed
+        self.dateGenerator = dateGenerator
+    }
+}
 
 
 class TrialPeriodTests: XCTestCase {
 
     // All test functions start with the word "test"
 
-    func testArrangeActAssert() {
-        // Arrange
-        let x = 20
-        let y = 40
-        let expected = 60
+//    func testArrangeActAssert() {
+//        // Arrange
+//        let x = 20
+//        let y = 40
+//        let expected = 60
+//
+//        // Act
+//        let actual = x + y
+//
+//        // Assert
+//        XCTAssertEqual(expected, actual)
+//    }
+    
+    //Make sure we can create a trial period
+    func testTrialPeriodToGetStarted() {
+        //Arrange
+        let expected = Date()
+        let timeTravelor = TimeTraveler(date: expected)
+        let trial = TrialPeriod(dateGenerator: timeTravelor.generateDate)
+
+        //Act
+        let actualDate = trial.dateInstalled
         
-        // Act
-        let actual = x + y
-        
-        // Assert
-        XCTAssertEqual(expected, actual)
+        //Assert
+        XCTAssertEqual(expected, actualDate)
     }
 }
